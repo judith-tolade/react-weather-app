55fe9a63eaa4166352465e64fbc915833c2b922b
judith-tolade <101555581+judith-tolade@users.noreply.github.com>
2022-06-25 02:23:25 +0100

    Added the file structure after installing the Terminal

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+export default function Weather() {
+  let weatherData = {
+    city: "Ibadan",
+    temperature: 72,
+    date: "Friday 10:00",
+    description: "Broken Clouds",
+    imgUrl: "https://ssl.gstatic.com/onebox/weather/64/sunny.png",
+    humidity: 99,
+    wind: 1,
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Weather">
+      <form>
+        <input
+          type="text"
+          placeholder="Type a City"
+          autofocus="on"
+          autocomplete="off"
+        />
+        <button type="submit" className="btn btn-info m-3">
+          {" "}
+          Search{" "}
+        </button>
+        <button type="button" className="btn btn-info">
+          Current
+        </button>
+      </form>
+      <div className="overview">
+        <h1>{weatherData.city}</h1>
+        <ul>
+          <li>Last updated: {weatherData.date}</li>
+          <li>{weatherData.description}</li>
+        </ul>
+      </div>
+      <div className="row">
+        <div className="col-6">
+          <ul>
+            <li>Humidity: {weatherData.humidity}%</li>
+            <li>Wind: {weatherData.wind} km/h</li>
+          </ul>
+        </div>
+        <div className="col-6">
+          <div className="clearfix weather-temperature">
+            <img
+              src={weatherData.imgUrl}
+              alt={weatherData.description}
+              className="float-left"
+            />
+            <div className="float-left">
+              <strong>{weatherData.temperature}</strong>
+              <span className="units">
+                <a href="/">°C</a> | <a href="/">°F</a>
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
